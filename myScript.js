236d504f1f09438ce7d299bd8a5f093f093f4f62
Coleman Grustas <chgrustas@gmail.com>
2025-06-08 15:25:17 -0400

    Add purpose statements to existing functions

--- myScript.js
+++ myScript.js
@@ -1,19 +1,42 @@
+let operand1;
+let operand2;
+let operator;
+
+// adds x and y
 function add(x, y) {
     return x + y;
 }
 
+// subtracts x and y
 function subtract(x, y) {
     return x - y;
 }
 
+// multiplies x and y
 function multiply(x, y) {
     return x * y;
 }
 
+// divides x and y
 function divide(x, y) {
     return x / y;
 }
 
-let operand1;
-let operand2;
-let operator;
+// function operate(x, operator, y) {
+//     switch (operator) {
+//         case '+':
+//             return add(x, y);
+//             break;
+//         case '-':
+//             return subtract(x, y);
+//             break;
+//         case '*':
+//             return multiply(x, y);
+//             break;
+//         case '/':
+//             return divide(x, y);
+//             break;
+//         default:
+//             alert("ERROR");
+//     }
+// }
