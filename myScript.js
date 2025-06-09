d4bdcdd17959cae24215d0fc88bb10628c7e8af6
Coleman Grustas <chgrustas@gmail.com>
2025-06-09 15:38:12 -0400

    Eliminate the displayContainsResult variable
    No need to track and update a separate boolean flag

--- myScript.js
+++ myScript.js
@@ -4,7 +4,6 @@ const Calculator = {
         operand1: "",
         operator: "",
         operand2: "",
-        displayContainsResult: false
     },
 
     init() {
@@ -52,15 +51,16 @@ const Calculator = {
     // Handle digit button clicks - appends digit to current operand and updates display
     // When a result is displayed, pressing a new digit starts a new calculation
     handleDigitClick(digit) {
-        if (!this.state.operator) {
-            if (this.state.displayContainsResult) {
-                this.state.operand1 = digit;
-                this.state.displayContainsResult = false;
-            } else {
-                this.state.operand1 += digit;
-            }
+        if (this.state.operator === "=") { // if we just completed a calculation
+            this.state.operand1 = digit;
+            this.state.operator = "";
             this.updateDisplay(this.state.operand1);
-        } else {
+        } 
+        else if (!this.state.operator) {
+            this.state.operand1 += digit;
+            this.updateDisplay(this.state.operand1);
+        } 
+        else {
             this.state.operand2 += digit;
             this.updateDisplay(this.state.operand2);
         }
@@ -68,22 +68,23 @@ const Calculator = {
 
     // Handle operator button clicks - performs pending operation if needed and sets new operator
     // Enables chain calculations by evaluating previous operation before setting new one
-    handleOperatorClick(operatorSymbol) {
+    handleOperatorClick(operator) {
         if (this.state.operand1) {
-            if (this.state.operand2) {
-                // TODO: should this section be delegated? 
+            if (this.state.operator === "=") { // if we just completed a calculation
+                this.state.operator = operator;
+                this.updateDisplay("");
+            } else if (this.state.operand2) {
                 const result = this.operate(
                     Number(this.state.operand1), 
                     this.state.operator, 
                     Number(this.state.operand2));
                 this.updateDisplay(result);
                 this.state.operand1 = result.toString();
-                this.state.operator = operatorSymbol;
+                this.state.operator = operator;
                 this.state.operand2 = "";
             } else {
-                this.state.displayContainsResult = false;
                 this.updateDisplay("");
-                this.state.operator = operatorSymbol;
+                this.state.operator = operator;
             }
         }
     },
@@ -99,9 +100,8 @@ const Calculator = {
             );
             this.updateDisplay(result);
             this.state.operand1 = result.toString();
-            this.state.operator = "";
+            this.state.operator = "=";
             this.state.operand2 = "";
-            this.state.displayContainsResult = true;
         }
     },
     
@@ -152,7 +152,6 @@ const Calculator = {
         this.state.operand1 = "";
         this.state.operator = "";
         this.state.operand2 = "";
-        this.state.displayContainsResult = false;
     },
     
      // Update the calculator display with the provided value
